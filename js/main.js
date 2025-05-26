f2684b12a3d4dca3e00291256e0586f929a97325
David Majholt <d.boy.dragoz@gmail.com>
2025-05-26 13:50:33 +0200

    JavaScript Search #23

--- js/main.js
+++ js/main.js
@@ -1,19 +1,42 @@
+// ------------------------ FETCHES ------------------------
+
+// ------------------------ CONSTS ------------------------
+
+const headerDOM = document.querySelector("#top-content")
 const burgerButtonDOM = document.querySelector("#burger-button")
 const burgerMenuDOM = document.querySelector("#search-form")
+const navSearchButtonDOM = document.querySelector("#navSearchButton")
 const navigationMenu = document.querySelector("#top-content .navigation")
 
+// ------------------------ EVENTS ------------------------
+
+navSearchButtonDOM.addEventListener("click", () => {
+	burgerMenuDOM.classList.toggle("none")
+
+	if (!burgerMenuDOM.classList.contains("none")) {
+		navSearchButtonDOM.style.fontWeight = "bold"
+		navSearchButtonDOM.style.textDecoration = "underline"
+	} else {
+		navSearchButtonDOM.style.fontWeight = ""
+		navSearchButtonDOM.style.textDecoration = ""
+	}
+
+})
+
 burgerButtonDOM.addEventListener("click", () => {
-	console.log("click")
 	if (navigationMenu.classList.contains("hiddenOnPhone")) {
 		navigationMenu.classList.remove("hiddenOnPhone")
+		headerDOM.style.minHeight = "fit-content"
 		burgerMenuDOM.classList.remove("none")
 	} else {
 		navigationMenu.classList.add("hiddenOnPhone")
+		headerDOM.style.minHeight = ""
 		burgerMenuDOM.classList.add("none")
 	}
 })
 
+// ------------------------ FUNCTIONS ------------------------
+
 function changePage(page) {
-	console.log("CLICK DETECTED")
 	document.location = page
 }
