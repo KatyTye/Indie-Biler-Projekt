987cfa4349a5f6f438ed7b062103afea6db25f25
David Majholt <d.boy.dragoz@gmail.com>
2025-05-27 09:43:53 +0200

    JavaScript Produkt Dialog #24

--- js/main.js
+++ js/main.js
@@ -12,42 +12,64 @@ fetch("/fetch/footer.html")
 		document.querySelector("#bottom-content").innerHTML = data
 	})
 
-// ------------------------ CONSTS ------------------------
+setTimeout(() => {
 
-const headerDOM = document.querySelector("#top-content")
-const burgerButtonDOM = document.querySelector("#burger-button")
-const burgerMenuDOM = document.querySelector("#search-form")
-const navSearchButtonDOM = document.querySelector("#navSearchButton")
-const navigationMenu = document.querySelector("#top-content .navigation")
 
-// ------------------------ EVENTS ------------------------
+	// ------------------------ CONSTS ------------------------
 
-navSearchButtonDOM.addEventListener("click", () => {
-	burgerMenuDOM.classList.toggle("none")
+	const headerDOM = document.querySelector("#top-content")
+	const burgerButtonDOM = document.querySelector("#burger-button")
+	const burgerMenuDOM = document.querySelector("#search-form")
+	const navSearchButtonDOM = document.querySelector("#navSearchButton")
+	const navigationMenu = document.querySelector("#top-content .navigation")
+	const modalMenu = document.querySelector("#product")
 
-	if (!burgerMenuDOM.classList.contains("none")) {
-		navSearchButtonDOM.style.fontWeight = "bold"
-		navSearchButtonDOM.style.textDecoration = "underline"
-	} else {
-		navSearchButtonDOM.style.fontWeight = ""
-		navSearchButtonDOM.style.textDecoration = ""
-	}
+	// ------------------------ EVENTS ------------------------
 
-})
+	navSearchButtonDOM.addEventListener("click", () => {
+		burgerMenuDOM.classList.toggle("none")
 
-burgerButtonDOM.addEventListener("click", () => {
-	if (navigationMenu.classList.contains("hiddenOnPhone")) {
-		navigationMenu.classList.remove("hiddenOnPhone")
-		headerDOM.style.minHeight = "fit-content"
-		burgerMenuDOM.classList.remove("none")
-	} else {
-		navigationMenu.classList.add("hiddenOnPhone")
-		headerDOM.style.minHeight = ""
-		burgerMenuDOM.classList.add("none")
-	}
-})
+		if (!burgerMenuDOM.classList.contains("none")) {
+			navSearchButtonDOM.style.fontWeight = "bold"
+			navSearchButtonDOM.style.textDecoration = "underline"
+		} else {
+			navSearchButtonDOM.style.fontWeight = ""
+			navSearchButtonDOM.style.textDecoration = ""
+		}
+
+	})
+
+	burgerButtonDOM.addEventListener("click", () => {
+		if (navigationMenu.classList.contains("hiddenOnPhone")) {
+			navigationMenu.classList.remove("hiddenOnPhone")
+			headerDOM.style.minHeight = "fit-content"
+			burgerMenuDOM.classList.remove("none")
+		} else {
+			navigationMenu.classList.add("hiddenOnPhone")
+			headerDOM.style.minHeight = ""
+			burgerMenuDOM.classList.add("none")
+		}
+	})
+}, 500)
+
+const modalMenu = document.querySelector("#product")
 
 // ------------------------ FUNCTIONS ------------------------
+
+async function openProductModal(id) {
+	const url = "/fetch/items.json"
+	const response = await fetch(url)
+	const json = await response.json()
+	modalMenu.showModal()
+	modalMenu.querySelector(".product-image").src = json[id - 1].url
+	modalMenu.querySelector("h2").innerHTML = json[id - 1].title
+	modalMenu.querySelector("h3").innerHTML = "<span>Pris:</span> " + json[id - 1].pris
+	modalMenu.querySelector("p").innerHTML = json[id - 1].beskrivelse
+}
+
+function closeProductModal() {
+	modalMenu.close()
+}
 
 function changePage(page) {
 	document.location = page
