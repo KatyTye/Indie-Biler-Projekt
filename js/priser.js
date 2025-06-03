5d1273fea07fce629ee4e3adef75d4a9c16a5714
David Majholt <d.boy.dragoz@gmail.com>
2025-06-03 10:49:00 +0200

    JavaScript Priser Search #33

--- js/priser.js
+++ js/priser.js
@@ -1,9 +1,51 @@
 const searchSectionDOM = document.querySelector("#search")
+const searchOutputOneDOM = document.querySelector("#search-output-one")
+const searchOutputTwoDOM = document.querySelector("#search-output-two")
 const filterSectionDOM = document.querySelector("#find")
 const filterFormDOM = document.querySelector("#filter-form")
 const filterOutputDOM = document.querySelector("#filter-output")
 const params = new URLSearchParams(location.search)
 const searchInput = params.get("search")
+
+async function search() {
+	const itemsFetch = await fetch("/fetch/items.json")
+	const items = await itemsFetch.json()
+
+	searchOutputOneDOM.querySelectorAll("div").forEach((elm) => {
+		elm.remove()
+	})
+
+	searchOutputTwoDOM.querySelectorAll("div").forEach((elm) => {
+		elm.remove()
+	})
+
+	items.forEach((obj) => {
+		let searchObj = true
+
+		if (!obj["title"].toLowerCase().includes(searchInput.toLowerCase())) {
+			console.log(obj["title"].toLowerCase())
+			if (!obj["beskrivelse"].toLowerCase().includes(searchInput.toLowerCase())) {
+				searchObj = false
+			}
+		}
+
+		if (searchObj) {
+			if (obj["type"] == 1) {
+				searchOutputOneDOM.innerHTML += `<div class="item" onclick="openProductModal(${obj["id"]})"><figure><img src="${obj["url"]}" alt="et produkt"></figure><section><h3>${obj["title"]}</h3><p>${obj["beskrivelse"]}</p></section><h3>${obj["pris"]} kr.</h3></div>`
+			} else {
+				searchOutputTwoDOM.innerHTML += `<div class="item" onclick="openProductModal(${obj["id"]})"><figure><img src="${obj["url"]}" alt="en bil"></figure><section><h3>${obj["title"]}</h3><p>${obj["beskrivelse"]}</p></section><h3>${obj["pris"]} kr.</h3></div>`
+			}
+		}
+
+		if (!searchOutputOneDOM.innerHTML.includes("<")) {
+			searchOutputOneDOM.innerHTML += "<p>Der kunne ikke findes nogle biludstur.</p>"
+		}
+
+		if (!searchOutputTwoDOM.innerHTML.includes("<")) {
+			searchOutputTwoDOM.innerHTML += "<p>Der kunne ikke findes nogle biler.</p>"
+		}
+	})
+}
 
 async function filterSearch() {
 	const itemsFetch = await fetch("/fetch/items.json")
@@ -42,6 +84,8 @@ async function filterSearch() {
 
 if (searchInput) {
 	filterSectionDOM.style.display = "none"
+
+	search()
 } else {
 	searchSectionDOM.style.display = "none"
 
